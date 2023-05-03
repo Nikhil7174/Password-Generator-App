34e1acc2be49b226b888e1232c0dab4f948bc0c9
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-05-04 01:33:47 +0530

    Added states and createPassword function

--- App.tsx
+++ App.tsx
@@ -1,7 +1,38 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React, {useState} from 'react';
 
+//Form validation
+import * as yup from 'yup';
+
+const passwordSchema = yup.object().shape({
+  passwordLength: yup
+    .number()
+    .min(4, 'Should be atleast of 4 characters')
+    .max(16, 'Should be atleast of 16 characters')
+    .required('Password is Required'),
+});
 export default function App() {
+  const [password, setPassword] = useState('');
+  const [isPassGenerated, setisPassGenerated] = useState(false);
+
+  const [lowerCase, setLowerCase] = useState(true);
+  const [upperCase, setUpperCase] = useState(false);
+  const [numbers, setNumbers] = useState(true);
+  const [symbols, setSymbols] = useState(true);
+
+  const generatePasswordString = (passwordLength: number) => {};
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  };
+
+  const resetPasswordState = () => {};
+
   return (
     <View>
       <Text>App</Text>
